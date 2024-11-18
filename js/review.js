14092bdba4e6465d55f5b5b22cfc041e9b4926eb
Joe <126Jhaney@frhsd.com>
2024-11-18 14:00:35 -0500

    gfhs

--- js/review.js
+++ js/review.js
@@ -2,26 +2,35 @@
 const d = new Date();
 // Store the value of getHours in variable
 let hour = d.getHours();
-hour =7;
 
-if (hour < 12) {
-// Morning
-// document.getElementById("greetings").innerText = "Good Morning";} 
-// document.getElementById("image").src = "img/Morning.jpg"
+// Hide all sections by default
+document.querySelector(".morning").classList.add("d-none");
+document.querySelector(".afternoon").classList.add("d-none");
 document.querySelector(".night").classList.add("d-none");
-  document.querySelector(".afternoon").classList.add("d-none");
+
+// Show the appropriate section based on the time of day
+if (hour < 12) {
+    // Morning
+    document.querySelector(".morning").classList.remove("d-none");
+} else if (hour < 17) {
+    // Afternoon
+    document.querySelector(".afternoon").classList.remove("d-none");
+} else {
+    // Night
+    document.querySelector(".night").classList.remove("d-none");
 }
-else if(hour < 17){
-// Afternoon
-// document.getElementById("greeting").innerText = "Good Afternoon";
-// document.getElementById("image").src = "img/Afternoon.jpg"
-document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".night").classList.add("d-none");
- }
-else {
-// Night
-// document.getElementById("greeting").innerText = "Good Night";
-// document.getElementById("image").src = "img/Night.jpg"
-document.querySelector(".morning").classList.add("d-none");
-  document.querySelector(".afternoon").classList.add("d-none");
-}
+
+// Add event listeners to buttons
+document.querySelectorAll('button[data-target]').forEach(button => {
+    button.addEventListener('click', function() {
+        const target = this.getAttribute('data-target');
+        
+        // Hide all sections
+        document.querySelector(".morning").classList.add("d-none");
+        document.querySelector(".afternoon").classList.add("d-none");
+        document.querySelector(".night").classList.add("d-none");
+        
+        // Show the selected section based on the button clicked
+        document.querySelector(`.${target}`).classList.remove("d-none");
+    });
+});
